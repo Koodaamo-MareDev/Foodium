ad465d7ec897cf4c8793e6288b96d1b8689c716f
MareDev <marcus.erkkila.me@gmail.com>
2025-05-31 22:25:14 +0300

    Update UpdateMicrowaveTimePacket.java

--- src/main/java/dev/koodaamo/foodium/network/UpdateMicrowaveTimePacket.java
+++ src/main/java/dev/koodaamo/foodium/network/UpdateMicrowaveTimePacket.java
@@ -1,41 +1,40 @@
 package dev.koodaamo.foodium.network;
 
-import net.minecraft.client.Minecraft;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.FriendlyByteBuf;
-import net.minecraft.network.protocol.Packet;
-import net.minecraft.network.protocol.PacketType;
-import net.minecraft.network.protocol.PacketUtils;
-import net.minecraft.network.protocol.game.ServerPacketListener;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraftforge.event.network.CustomPayloadEvent;
 
-public class UpdateMicrowaveTimePacket implements Packet<ServerPacketListener>{
+public class UpdateMicrowaveTimePacket {
 
-	@Override
-	public PacketType<? extends Packet<ServerPacketListener>> type() {
-		return null;
-	}
-	
 	private final BlockPos pos;
-    private final int time;
+	private final int time;
 
-    public UpdateMicrowaveTimePacket(BlockPos pos, int time) {
-        this.pos = pos;
-        this.time = time;
-    }
+	public UpdateMicrowaveTimePacket(BlockPos pos, int time) {
+		this.pos = pos;
+		this.time = time;
+	}
 
-    public UpdateMicrowaveTimePacket(FriendlyByteBuf buf) {
-        this.pos = buf.readBlockPos();
-        this.time = buf.readInt();
-    }
+	public UpdateMicrowaveTimePacket(FriendlyByteBuf buf) {
+		this.pos = buf.readBlockPos();
+		this.time = buf.readInt();
+	}
 
-    public void toBytes(FriendlyByteBuf buf) {
-        buf.writeBlockPos(pos);
-        buf.writeInt(time);
-    }
+	public void encode(FriendlyByteBuf buf) {
+		buf.writeBlockPos(pos);
+		buf.writeInt(time);
+	}
 
-	@Override
-	public void handle(ServerPacketListener listener) {
-        PacketUtils.ensureRunningOnSameThread(this, listener, Minecraft.getInstance());
+	public void handle(CustomPayloadEvent.Context context) {
+		// Skip handling on client - shouldn't happen for now but better safe than sorry
+		if (context.isClientSide()) {
+			return;
+		}
+
+		// Don't process on network thread - wrapping code in this ensures thread safety.
+		context.enqueueWork(() -> {
+			// TODO: Handle packet
+		});
 	}
 
 }
